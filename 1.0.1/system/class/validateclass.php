71f573ae7336d4f12ffb075845a668d54d853bf9
Ahsan Zahid Chowdhury <azc.pavel@gmail.com>
2014-05-07 10:49:35 +0600

    Update validateclass.php

--- 1.0.1/system/class/validateclass.php
+++ 1.0.1/system/class/validateclass.php
@@ -1,8 +1,14 @@
 <?php
-
-/**
-* 
+/*
+*	@author : Ahsan Zahid Chowdhury
+*	@owner : Exception Solutions
+*	@email : azc.pavel@gmail.com
+*	@phone : +880 1677 533818
+*	@since : 2014-04-20
+*	@version : 1.0.1
+*	Validate Rules Class
 */
+
 Final class ValidateClass
 {
 	private $required = 1;
