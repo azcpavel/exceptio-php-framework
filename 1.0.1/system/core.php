2cad49c8b32e13926026b2ebfb381d98a7892706
Ahsan Zahid Chowdhury <azc.pavel@gmail.com>
2014-05-07 10:34:32 +0600

    Update core.php

--- 1.0.1/system/core.php
+++ 1.0.1/system/core.php
@@ -1,5 +1,13 @@
 <?php
-
+/*
+*	@author : Ahsan Zahid Chowdhury
+*	@owner : Exception Solutions
+*	@email : azc.pavel@gmail.com
+*	@phone : +880 1677 533818
+*	@since : 2014-04-20
+*	@version : 1.0.1
+*	Core functions
+*/
 
 function __autoload($class)
 {
