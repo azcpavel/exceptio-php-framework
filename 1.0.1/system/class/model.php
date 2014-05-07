f51616882f89f261a546ce1a327a91c95c20dad6
Ahsan Zahid Chowdhury <azc.pavel@gmail.com>
2014-05-07 10:46:14 +0600

    Update model.php

--- 1.0.1/system/class/model.php
+++ 1.0.1/system/class/model.php
@@ -1,7 +1,14 @@
 <?php
-/**
-* 
+/*
+*	@author : Ahsan Zahid Chowdhury
+*	@owner : Exception Solutions
+*	@email : azc.pavel@gmail.com
+*	@phone : +880 1677 533818
+*	@since : 2014-04-20
+*	@version : 1.0.1
+*	Base Model
 */
+
 abstract class Ex_Model
 {
 
