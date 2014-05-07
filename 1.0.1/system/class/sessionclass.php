038c3b27fdc47126a2a7269694439c6d543060a4
Ahsan Zahid Chowdhury <azc.pavel@gmail.com>
2014-05-07 10:48:57 +0600

    Update sessionclass.php

--- 1.0.1/system/class/sessionclass.php
+++ 1.0.1/system/class/sessionclass.php
@@ -1,7 +1,14 @@
 <?php
-/**
-* 
+/*
+*	@author : Ahsan Zahid Chowdhury
+*	@owner : Exception Solutions
+*	@email : azc.pavel@gmail.com
+*	@phone : +880 1677 533818
+*	@since : 2014-04-20
+*	@version : 1.0.1
+*	Get or Set Session with or without Database
 */
+
 Final class SessionClass
 {
 	private $sess_db;
