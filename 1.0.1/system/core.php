3b063412fd2c3c91b4d1e8e49f75500a27ffd65b
Ahsan Zahid Chowdhury <azc.pavel@gmail.com>
2014-05-21 00:45:53 +0600

    Update core.php

--- 1.0.1/system/core.php
+++ 1.0.1/system/core.php
@@ -74,6 +74,52 @@ function truncate_str($str, $maxlen) {
 	return $newstr;
 }
 
+function print_thousand($num, $dec = 2)
+	{
+		if($num > 3)
+		{
+			$last_part = substr($num, -3, 3);
+			$first_part = substr($num, 0, -3);
+			$final = '';
+			$final_first = '';
+			
+			if(strlen($first_part) > $dec*3)
+			{
+				$final_first = substr($first_part, 0, -$dec*3).',';
+				$first_part = substr($first_part, -$dec*3, $dec*3);
+
+			}		
+
+			$array = str_split($first_part);
+
+			$flage = 0;
+			$count = count($array);				
+
+			$interval = 1;
+			$count_coma = 1;
+			$final = ','.$last_part;
+			for ($i=$count-1; $i >= 0; $i--) { 
+				$final = $array[$i].$final;
+				
+					if($count_coma == $dec && $interval <= 3)
+						{
+							$final = ','.$final;
+							$interval ++;
+							$count_coma=0;
+						}
+					$count_coma++;
+
+			}
+
+			$tmp_first = str_split($final);
+			if($tmp_first[0]==',')
+				$final = substr($final, 1);
+			return $final_first.$final;
+		}
+		else
+			return $num;
+}
+
 function &get_controller_instance()
 {
 	return ex_controller::get_all_instance();
