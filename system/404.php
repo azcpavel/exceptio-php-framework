734a0b109f54164ae90b05d3a839da5cd8c24a7f
azcpavel <azc.pavel@gmail.com>
2018-12-22 16:15:18 +0600

    404 Update

--- system/404.php
+++ system/404.php
@@ -7,6 +7,6 @@
 	<h1>Not Found</h1>
 	<p>The requested URL <?php echo BASEHOST.$_SERVER['REQUEST_URI']; ?> was not found on this server.</p>
 	<hr>		
-	<address><?php echo "$_SERVER[SERVER_SIGNATURE]"; ?></address>
+	<address><?php echo (isset($_SERVER['SERVER_SIGNATURE'])) ? $_SERVER['SERVER_SIGNATURE'] : ""; ?></address>
 	</body>
 </html>
